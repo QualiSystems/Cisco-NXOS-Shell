fc9a47de0aa69f18d11aa988cb3a04761244d6ab
Sergii Shmarkatiuk <sergey.shmarkatyuk@gmail.com>
2016-04-29 16:11:47 +0300

    Removed DEFAULT_ACTIONS reference

--- cloudshell/networking/cisco/nxos/cisco_nxos_configuration.py
+++ cloudshell/networking/cisco/nxos/cisco_nxos_configuration.py
@@ -7,5 +7,3 @@ HANDLER_CLASS = CiscoNXOSHandler
 DEFAULT_PROMPT = '.*[#>]\s*$'
 ENABLE_PROMPT = '.*# *$'
 CONFIG_MODE_PROMPT = '\(config.*\)# *$'
-
-# DEFAULT_ACTIONS = CiscoNXOSHandler._default_actions()
