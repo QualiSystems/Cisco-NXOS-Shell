9fd69bc3897fa02a90212101f1600fd9afeffbce
Sergii Shmarkatiuk <sergey.shmarkatyuk@gmail.com>
2016-05-12 16:06:51 +0300

    Removed commented lines

--- cloudshell/networking/cisco/nxos/cisco_nxos_handler.py
+++ cloudshell/networking/cisco/nxos/cisco_nxos_handler.py
@@ -8,24 +8,3 @@ class CiscoNXOSHandler(CiscoHandlerBase):
     def __init__(self):
         CiscoHandlerBase.__init__(self)
         self.supported_os = ['NXOS', 'NX-OS']
-
-    # def _default_actions(self):
-    #     """Send default commands to configure/clear session outputs
-    #     :return:
-    #     """
-    #     self._session.set_unsafe_mode(True)
-    #
-    #     output = self._send_command('')
-    #
-    #     if re.search('> *$', output):
-    #         output = self._send_command('enable',
-    #                                     expected_map={'[Pp]assword': expected_actions.send_default_password})
-    #
-    #     if re.search('> *$', output):
-    #         raise Exception('Cisco OS', "Can't set enable mode!")
-    #
-    #     self._set_terminal_length(0)
-    #
-    #     self._enter_configuration_mode()
-    #     self._send_command('no logging console')
-    #     self._exit_configuration_mode()
